c51334922f4cf8b8bbd0901af21ec0c96d1f856a
hkarambizi <hkarambizi@gmail.com>
2017-03-20 18:07:27 -0400

    part 1: created Tamagotchi constructor

--- unit_02/w07d02/homework/tamagotchi.js
+++ unit_02/w07d02/homework/tamagotchi.js
@@ -1,7 +1,16 @@
 console.log('tamagotchi file is loaded');
 
 //your constructor function here
-
+function Tamagotchi(foodInTummy, restedness, health) {
+	this.foodInTummy = 10;
+	this.restedness = 10;
+	this.health = 10;
+	this.cry = function() {
+		this.foodInTummy--;
+		console.log(this.foodInTummy);
+		console.log("WAAAHH!!!!");
+	}
+};
 
 //create new Tamagotchis
 
--- unit_02/w07d02/student_labs/promises-lab-starter/readFile.js
+++ unit_02/w07d02/student_labs/promises-lab-starter/readFile.js
@@ -9,8 +9,7 @@ fs.readFile(inFile, { encoding: 'utf8' }, function(error, content) {
     console.error(error);
   }
 
-  // 'Billy\nJames\nNick\n' --> ['Billy', 'James', 'Nick']
-
+  // 'Billy\nJames\nNick\n' --> ['Billy', 'James', 'Nick'
   let lines = content.split('\n');
 
   // clean up the array by removing the empty line
@@ -20,3 +19,15 @@ fs.readFile(inFile, { encoding: 'utf8' }, function(error, content) {
     console.log('Hello, ' + line + '!');
   });
 });
+
+var promiseToReadFile = new Promise(function(resolve, reject) {
+   let lines = content.split('\n');
+   lines.pop();
+ });
+  promiseToReadFile
+    .then(lines.forEach(function(line) {
+    console.log('Hello, ' + line + '!');
+  })
+    .catch(function(error) {
+      console.error(error);
+    })
