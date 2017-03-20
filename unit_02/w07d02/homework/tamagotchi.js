2140ef153357928800387218a28e23cedf5a0f8b
hkarambizi <hkarambizi@gmail.com>
2017-03-20 18:41:28 -0400

    Edited my constructor to change the only arguments to name and creatureType in order to complete next step

--- unit_02/w07d02/homework/tamagotchi.js
+++ unit_02/w07d02/homework/tamagotchi.js
@@ -1,7 +1,9 @@
 console.log('tamagotchi file is loaded');
 
 //your constructor function here
-function Tamagotchi(foodInTummy, restedness, health) {
+function Tamagotchi(name, creatureType) {
+	this.name = name;
+	this.type = creatureType;
 	this.foodInTummy = 10;
 	this.restedness = 10;
 	this.health = 10;
@@ -11,20 +13,23 @@ function Tamagotchi(foodInTummy, restedness, health) {
 		console.log("WAAAHH!!!!. This tamagotchi is now crying!");
 	}
 	this.puke = function() {
-		this.food
+		this.foodInTummy--;
+		console.log("This tamagotchi has this much food in tummy: " + this.foodInTummy);
+		console.log("BoooHooo!!. This tamagotchi puked and is now crying!");
+	}
+	this.yawn = function() {
+		this.restedness--;
+		console.log(this.name + "has current restedness of: " + this.restedness);
 	}
 };
 
+
 //create new Tamagotchis
 //1st Tamagotchi named "Shirley"
-var earl = new Tamagotchi();
-	earl.name = "Earl";
-	earl.creatureType = "Drunkard";
+var earl = new Tamagotchi('Earl', 'Drunkard');
 	earl.cry();
 //2nd Tamagotchi named "Shirley"
-var shirley = new Tamagotchi();
-	shirley.name = "Shirley";
-	shirley.creatureType = "Sheriff";
+var shirley = new Tamagotchi('Shirley', 'Sheriff');
 	shirley.cry();
 
 
