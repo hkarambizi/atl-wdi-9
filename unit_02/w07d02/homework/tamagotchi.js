2ab5c917773fcd39b18093bc7280d5f0071d3761
hkarambizi <hkarambizi@gmail.com>
2017-03-20 18:21:10 -0400

    part 2: added arguments to Tamagotchi

--- unit_02/w07d02/homework/tamagotchi.js
+++ unit_02/w07d02/homework/tamagotchi.js
@@ -7,12 +7,20 @@ function Tamagotchi(foodInTummy, restedness, health) {
 	this.health = 10;
 	this.cry = function() {
 		this.foodInTummy--;
-		console.log(this.foodInTummy);
-		console.log("WAAAHH!!!!");
+		console.log("This tamagotchi has this much food in tummy: " + this.foodInTummy);
+		console.log("WAAAHH!!!!. This tamagotchi is now crying!");
 	}
 };
 
 //create new Tamagotchis
+//1st Tamagotchi named "Shirley"
+var earl = new Tamagotchi();
+	earl.name = "Earl";
+	earl.cry();
+//2nd Tamagotchi named "Shirley"
+var shirley = new Tamagotchi();
+	shirley.name = "Shirley";
+	shirley.cry();
 
 
 //test out your Tamagotchies below via console.logs
