2ca5cba1b8aee3abf10172b36f888b277238252d
hkarambizi <hkarambizi@gmail.com>
2017-03-23 19:23:51 -0400

    added authentication authorization middleware

--- unit_02/w08d01/student_labs/auth_starter/controllers/users.js
+++ unit_02/w08d01/student_labs/auth_starter/controllers/users.js
@@ -34,7 +34,6 @@ router.get('/:id', function(req, res) {
   .exec(function(err, user) {
     if (err) console.log(err);
     console.log(user);
-    // res.render('user/show.hbs', { user: user } );
     res.render('users/show.hbs', { user: user } );
   });
 });
